4c1276ee47fbf789ce8db113e327e3279b9bb4b5
Rodrigo Cardozo - T2F <rodrigodasilvacardozo@gmail.com>
2022-09-21 18:38:37 -0300

    att

--- trabalho 1.py
+++ trabalho 1.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 n = 5
 L = 0.1
 delx = L/n
@@ -7,29 +6,22 @@ Ti = 100
 Tf = 20
 K = np.zeros(n)
 T = np.zeros(n)
-
 T[0] = 100
 T[n-1] = 20
-
-erro = 10
 j = 0
-while j < 10000:
+while j < 100:
     for i in range(0, n, 1):
         K[i] = 1.6 - 0.01*T[i]
     for i in range(1, n-1, 1):
         T[i] = -(T[i+1]*K[i] + T[i-1]*K[i-1])/(K[i] - 2*K[i] - K[i-1])
-
     j = j + 1
 print(type(T))
-
 x = np.linspace(0,0.1,n)
 Temp = np.linspace(0,n,n)
 for i in range(0, len(x), 1):
     Temp[i] = 160 - np.sqrt(3600 + 160000*x[i])
 print(type(Temp))
-
 import matplotlib.pyplot as plt
-
 plt.plot(x, T, linestyle='--', marker='o', color = 'blue', markersize = 4)
 plt.plot(x, Temp, linestyle='--', marker='o', color = 'red', markersize = 4)
 plt.xlabel('Distânia [m]', fontsize=15)
