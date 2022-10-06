306a8a586384526f9b583eb8ae0f0bb14dbaebf9
Rodrigo Cardozo - T2F <rodrigodasilvacardozo@gmail.com>
2022-10-06 16:19:09 -0300

    atualizacao trabalho 2

---
+++ Trabalho 2 - RP.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pandas as pd
+
+k = 200
+rho = 7000
+cp = 500
+a = 1
+b = 1
+n = 50
+n_tempo = 10
+delx = a/n
+dely = b/n
+delz = 1
+
+T_superior = 100
+T_inferior = 20
+T_direita = 20
+T_esquerda = 20
+T = np.zeros([n,n])
+for i in range(0, n, 1):
+    for k in range(0, n, 1):
+        T[i][k] = 20
+err = 10
+Erro = []
+while err > 0.0001:
+    A = T[0][0]
+    for i in range(0, n, 1):
+
+        for j in range(0, n, 1):
+
+            if i == 0 and j == 0:
+                T_e = T_esquerda
+                T_n = T_superior
+                T_s = T[i + 1][j]
+                T_w = T[i][j + 1]
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if i == 0 and n - 1 > j > 0:
+                T_e = T[i][j - 1]
+                T_n = T_superior
+                T_s = T[i + 1][j]
+                T_w = T[i][j + 1]
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if i == 0 and j == n - 1:
+                T_e = T[i][j - 1]
+                T_n = T_superior
+                T_s = T[i + 1][j]
+                T_w = T_direita
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if n - 1 > i > 0 and j == 0:
+                T_e = T_esquerda
+                T_n = T[i - 1][j]
+                T_s = T[i + 1][j]
+                T_w = T[i][j + 1]
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if n - 1 > i > 0 and n - 1 > j > 0:
+                T_e = T[i][j - 1]
+                T_n = T[i - 1][j]
+                T_s = T[i + 1][j]
+                T_w = T[i][j + 1]
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if n - 1 > i > 0 and j == n - 1:
+                T_e = T[i][j - 1]
+                T_n = T[i - 1][j]
+                T_s = T[i + 1][j]
+                T_w = T_direita
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if i == n - 1 and j == 0:
+                T_e = T_esquerda
+                T_n = T[i - 1][j]
+                T_s = T_inferior
+                T_w = T[i][j + 1]
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if i == n - 1 and n - 1 > j > 0:
+                T_e = T[i][j - 1]
+                T_n = T[i - 1][j]
+                T_s = T_inferior
+                T_w = T[i][j + 1]
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+
+            if i == n - 1 and j == n - 1:
+                T_e = T[i][j - 1]
+                T_n = T[i - 1][j]
+                T_s = T_inferior
+                T_w = T_direita
+                T[i][j] = (T_w + T_e + T_n + T_s)/4
+    err = np.sqrt((A-T[0][0])**2)
+    Erro.append(err)
+
+# balanco de energia
+Q_ver = []
+Q_hor = []
+for i in range(0, n-1, 1):
+    for k in range(0, n-1, 1):
+        q_inVert = k*abs(T[i][k] - T[i][k+1])
+        q_inHori = k*abs(T[i][k] - T[i+1][k])
+        Q_ver.append(q_inVert)
+        Q_hor.append(q_inHori)
+print(sum(Q_ver) + sum(Q_hor))
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.imshow(T, cmap='hot', interpolation='nearest')
+plt.show()
--- Trabalho 2.py
+++ Trabalho 2.py
@@ -1,20 +1,19 @@
 
 import numpy as np
 
-k = 200
+k = 20
 rho = 7000
 cp = 500
 a = 1
 b = 1
-n = 10
-n_tempo = 1000
+n = 5
+
 delx = a/n
 dely = b/n
 delz = 1
 
-tempo_total = 2000
-delt = tempo_total/n_tempo
-Tempo = np.linspace(delt, tempo_total, n_tempo )
+delt = rho*cp*delx**2/(2*k*n*0.5)
+Tempo = 0
 T_old = np.zeros([n,n])
 T = np.zeros([n,n])
 for i in range(0, n, 1):
@@ -24,12 +23,13 @@ T_superior = 100
 T_inferior = 20
 T_direita = 20
 T_esquerda = 20
+err = 10
+cont = 1
 
-for t in range(0, len(Tempo), 1):
-
-    apx = (k*Tempo[t])/(rho*cp*delx**2)
-    apy = (k*Tempo[t])/(rho*cp*delx**2)
-
+while err > 0.0001:
+    apx = (k*delt)/(rho*cp*delx**2)
+    apy = (k*delt)/(rho*cp*delx**2)
+    A = T[0][0]
     for i in range(0, n, 1):
 
         for j in range(0, n, 1):
@@ -96,5 +96,32 @@ for t in range(0, len(Tempo), 1):
                 T_s = T_inferior
                 T_w = T_direita
                 T[i][j] = T_old[i][j] + apx*(T_w + T_e - 2*T_old[i][j]) + apy*(T_n + T_s - 2*T_old[i][j])
+
+    err = np.sqrt((A-T[0][0])**2)
     T_old = T
-print(T)
+    cont += 1
+    Tempo = Tempo + delt
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+plt.imshow(T, cmap='hot', interpolation='nearest')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
